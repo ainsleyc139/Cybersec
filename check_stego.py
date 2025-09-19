7202b20ca55a524af9908ec67a27a87eb3476a57
bboypromb <144021145+bboypromb@users.noreply.github.com>
2025-09-19 14:42:48 +0800

    Added new encode/decode codes. WE can decode without the need to input LSB and Coordinates.

--- check_stego.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import numpy as np
-
-orig = cv2.imread("input.bmp")
-encoded = cv2.imread("encoded_output.bmp")
-
-diff = np.sum(orig != encoded)
-print(f"Number of pixels changed: {diff}")
--- fake_payload_txt.py
+++
@@ -1,2 +0,0 @@
-with open("big_test.txt", "w") as f:
-    f.write("HELLO_WORLD\n" * 1_000_000)  # repeat 1 million times

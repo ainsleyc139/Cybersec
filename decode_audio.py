180fe81035930175d2a44f5311d750b3dab9057e
Student-dot-jpg <2302960@sit.singaporetech.edu.sg>
2025-09-21 21:26:50 +0800

    Adds option to use key, gif as payload and fixes errors

--- decode_audio.py
+++ decode_audio.py
@@ -1,6 +1,6 @@
-import cv2
 import numpy as np
 import wave
+import hashlib
 
 def to_bin(data):
     """Convert data into binary format as string/list"""
@@ -12,38 +12,6 @@ def to_bin(data):
         return format(int(data), "08b")
     else:
         raise TypeError(f"Type not supported: {type(data)}")
-
-
-def decode(image_name, n_bits=1):
-    print("[+] Decoding...")
-    image = cv2.imread(image_name)
-    if image is None:
-        raise FileNotFoundError(f"Could not open {image_name}. Check path and extension.")
-
-    binary_data = ""
-    decoded_data = ""
-    stop_marker = "====="
-
-    for row in image:
-        for pixel in row:
-            # unpack each channel (B, G, R)
-            for channel in range(3):
-                # take the last n_bits from each channel
-                binary_data += to_bin(pixel[channel])[-n_bits:]
-
-                # decode whenever we have at least 8 bits
-                while len(binary_data) >= 8:
-                    byte = binary_data[:8]
-                    binary_data = binary_data[8:]
-                    decoded_data += chr(int(byte, 2))
-
-                    # stop when marker is found
-                    if decoded_data.endswith(stop_marker):
-                        return decoded_data[:-len(stop_marker)]
-
-    print("[!] Warning: stop marker not found")
-    return decoded_data
-
 
 def decode_audio_header(binary_data):
     
@@ -59,7 +27,19 @@ def decode_audio_header(binary_data):
     
     return header_data, header_length
 
-def decode_audio(file_name, n_bits=1, start_time=None, end_time=None):
+def to_float_or_none(x):
+    if x is None: return None
+    s = str(x).strip()
+    if s == "": return None
+    return float(s)
+
+def hash_to_seed(key_text: str) -> int:
+    if not isinstance(key_text, str) or not key_text.strip():
+        raise ValueError("Key/passphrase is required.")
+    return int.from_bytes(hashlib.sha256(key_text.encode("utf-8")).digest()[:8], "little", signed=False)
+
+def decode_audio(file_name, n_bits=1):
+    n_bits = int(n_bits)
     print("[+] Decoding...")
     with wave.open(file_name, "rb") as audio:
         sample_rate = audio.getframerate()
@@ -72,60 +52,147 @@ def decode_audio(file_name, n_bits=1, start_time=None, end_time=None):
     
     frame_size = num_channels * sample_width
 
-    if start_time is not None and end_time is not None:
-        start_frame = int(start_time * sample_rate)
-        end_frame = int(end_time * sample_rate)
-        # calculate start and end byte
-        start_byte = start_frame * frame_size
-        end_byte = end_frame * frame_size
-        end_byte = min(end_byte,len(frames))
-    else:
-        start_byte =0
-        end_byte = len(frames)
-
     binary_data = ""
     decoded_data = ""
-    stop_marker = "====="
 
-    for i in range(start_byte,end_byte):
+    # convert stop marker to binary so we know where to stop decoding
+    stop_marker = "====="
+    binary_stop_marker = to_bin(stop_marker)
+   
+    # same for the starting characters of the header '<type:'
+    marker = "<type:" 
+    binary_header_marker = to_bin(marker)
+
+    bits_buffer = []
+    end_marker_len = len(binary_stop_marker)
+
+    for i in range(0,len(frames)):
         byte = frames[i]
+
         # take the last n_bits from each byte in frames, convert to string
-        binary_data += format(byte & ((1 << n_bits) - 1), f'0{n_bits}b')
-        
-    header_data, header_length = decode_audio_header(binary_data)
+        bits = byte & ((1 << n_bits) - 1)
+        bits_buffer.append(f'{bits:0{n_bits}b}')
+
+        #check for stop marker in the last few bits
+        if (''.join(bits_buffer[-end_marker_len:]) == binary_stop_marker):
+            # Remove the marker bits
+            bits_buffer = bits_buffer[:-end_marker_len]
+            break
+
+    binary_data = ''.join(bits_buffer)
+
+    # search for the header in the binary data by finding the marker
+    header_start = binary_data.find(binary_header_marker)
+
+    # decode the header data '<type: ;size: >'
+    header_data, header_length = decode_audio_header(binary_data[header_start:])
     header_parts = header_data.strip("<>").split(';')
     payload_extension = '.' + header_parts[0][5:]
-    payload_size = header_parts[1][5:]
-
+    payload_size = int(header_parts[1][5:])
+    
+    # If the payload is not a file, extension is put as .nil when encoding
     if payload_extension == ".nil":
         payload_data = binary_data[header_length:]
         while len(payload_data) >= 8:
             byte_data = payload_data[:8]
             payload_data = payload_data[8:]
             decoded_data += chr(int(byte_data, 2))
-            
-            # stop when marker is found
-            if decoded_data.endswith(stop_marker):
-                secret_message = decoded_data[:-len(stop_marker)]
-                print(f"[+] Hidden message extracted:\n{secret_message}")
-        print("[!] Warning: stop marker not found")
-        
-        
+    
     else:
-
-        file_bits = binary_data[header_length:header_length + int(payload_size) * 8]
+        # decode payload for files
+        header_length = header_start + header_length
+        file_bits = binary_data[header_length:header_length + payload_size * 8]
         file_bytes = bytearray(int(file_bits[i:i+8], 2) for i in range(0, len(file_bits), 8))
 
         with open(f"decoded_file{payload_extension}", "wb") as f:
             f.write(file_bytes)
         print(f"[+] File saved as decoded_file{payload_extension}")
     
+def decode_audio_with_key(file_name, key, start_time, end_time, n_bits=1):
+    n_bits = int(n_bits)
+    print("[+] Decoding...")
+    with wave.open(file_name, "rb") as audio:
+        sample_rate = audio.getframerate()
+        sample_width = audio.getsampwidth()
+        num_channels = audio.getnchannels()
+        frames = bytearray(audio.readframes(audio.getnframes()))
+
+    if frames is None:
+        raise FileNotFoundError(f"Could not open {file_name}. Check path and extension.")
+    
+    frame_size = num_channels * sample_width
+    total_bytes = len(frames)
+
+    
+    # convert user input time
+    start_time = to_float_or_none(start_time)
+    end_time = to_float_or_none(end_time)
+
+    #convert time into frames, then into bytes
+    if start_time is not None and end_time is not None:
+        start_frame = int(start_time * sample_rate)
+        end_frame = int(end_time * sample_rate)
+        # calculate start and end byte
+        start_byte = start_frame * frame_size
+        end_byte = end_frame * frame_size
+        if end_byte > total_bytes:
+            raise ValueError("[!] End time exceeds file length")
+        max_bits = (end_byte - start_byte) * n_bits
+    else:
+        raise ValueError("[!] start and end times must be entered")
+
+    # Build shuffled order using same PRNG/seed as your encoder
+    order = np.arange(start_byte, end_byte, dtype = np.int64)
+    seed = hash_to_seed(key)
+    rng = np.random.Generator(np.random.PCG64(seed))
+    rng.shuffle(order)
+
+    bits_buffer =[]
+    for pos in order:
+        bits = frames[pos] & ((1 << n_bits ) - 1 )
+        bits_buffer.append(f'{bits:0{n_bits}b}')
+    
+    binary_data =''.join(bits_buffer)
+    
+    # front of header as a market to find it
+    marker = "<type:" 
+    binary_header_marker = to_bin(marker)
+
+    decoded_data = ""
+
+    # search for the header in the binary data by finding the marker
+    header_start = binary_data.find(binary_header_marker)
+
+    # decode the header data '<type: ;size: >'
+    header_data, header_length = decode_audio_header(binary_data[header_start:])
+    header_parts = header_data.strip("<>").split(';')
+    payload_extension = '.' + header_parts[0][5:]
+    payload_size = int(header_parts[1][5:])
+
+    
+    # If the payload is not a file, extension is put as .nil when encoding
+    if payload_extension == ".nil":
+        payload_data = binary_data[header_length:]
+        while len(payload_data) >= 8:
+            byte_data = payload_data[:8]
+            payload_data = payload_data[8:]
+            decoded_data += chr(int(byte_data, 2))
+        decoded_data = decoded_data.removesuffix("=====")
+        print('[+] The secret message is: '+ decoded_data)
+    
+    else:
+        # decode payload for files
+        header_length = header_start + header_length
+        file_bits = binary_data[header_length:header_length + payload_size * 8]
+        file_bytes = bytearray(int(file_bits[i:i+8], 2) for i in range(0, len(file_bits), 8))
+
+        with open(f"decoded_file{payload_extension}", "wb") as f:
+            f.write(file_bytes)
+        print(f"[+] File saved as decoded_file{payload_extension}")
 
 if __name__ == "__main__":
     print("=== LSB Steganography Decoder ===")
     input_file = input("Enter encoded  filename: ").strip()
-    #if not input_file.lower().endswith(".bmp"):
-        #input_file += ".bmp"
 
     while True:
         try:
@@ -133,14 +200,18 @@ if __name__ == "__main__":
             if 1 <= n_bits <= 8:
                 break
             else:
-                print("⚠️ Please enter a number between 1 and 8.")
+                print("! Please enter a number between 1 and 8.")
         except ValueError:
-            print("⚠️ Invalid input. Enter a number between 1 and 8.")
-
+            print("! Invalid input. Enter a number between 1 and 8.")
+    
     if input_file.endswith(".wav"):
-        start = input('Enter start time in seconds:')
-        end = input('Enter end time in seconds:')
-        hidden_message = decode_audio(input_file, n_bits,start,end)
+        use_key = input("use a secret key? (Y/N) ").strip()
+        if use_key.lower() == "y":
+            key = input("enter secret key: ").strip()
+            start = input("Enter start time in seconds: ").strip()
+            end = input("enter end time in seconds: ").strip()
+            decode_audio_with_key(input_file, key, start, end, n_bits)
+        else: 
+            decode_audio(input_file, n_bits)
     else:
-        hidden_message = decode(input_file, n_bits)
-        print("[+] Hidden Message:", hidden_message)
+        print("Please enter a .wav file")
--- encode_audio.py
+++ encode_audio.py
@@ -1,6 +1,6 @@
-import cv2
 import numpy as np
 import wave
+import hashlib
 
 def to_bin(data):
     """Convert data into binary format as string"""
@@ -13,48 +13,27 @@ def to_bin(data):
     else:
         raise TypeError(f"Type not supported: {type(data)}")
 
+# for times
+def to_float_or_none(x):
+    if x is None: return None
+    s = str(x).strip()
+    if s == "": return None
+    return float(s)
 
-def encode(image_name, secret_data, output_name, n_bits=1):
-    image = cv2.imread(image_name)
-    n_bytes = image.shape[0] * image.shape[1] * 3 * n_bits // 8
-    print(f"[*] Maximum bytes to encode with {n_bits} LSB(s): {n_bytes}")
 
-    # add stop marker
-    secret_data += "====="
-    binary_secret_data = to_bin(secret_data)
-    data_len = len(binary_secret_data)
-
-    print(f"[*] Encoding message of {len(secret_data)} characters ({data_len} bits)")
-
-    if data_len > image.size * n_bits:
-        raise ValueError("[!] Insufficient bytes, need bigger image or fewer bits per channel.")
-
-    print("[*] Encoding data...")
-    data_index = 0
-
-    for row in image:
-        for pixel in row:
-            for channel in range(3):  # B, G, R
-                if data_index < data_len:
-                    mask = ~((1 << n_bits) - 1) & 0xFF
-                    bits = int(binary_secret_data[data_index:data_index+n_bits].ljust(n_bits, '0'), 2)
-                    pixel[channel] = np.uint8((int(pixel[channel]) & mask) | bits)
-                    data_index += n_bits
-                if data_index >= data_len:
-                    break
-            if data_index >= data_len:
-                break
-        if data_index >= data_len:
-            break
-
-    cv2.imwrite(output_name, image)
-    print(f"[+] Data encoded successfully into {output_name}")
+def hash_to_seed(key_text: str) -> int:
+    if not isinstance(key_text, str) or not key_text.strip():
+        raise ValueError("Key/passphrase is required.")
+    return int.from_bytes(hashlib.sha256(key_text.encode("utf-8")).digest()[:8], "little", signed=False)
 
 def get_payload(file_path, is_file):
 
     if is_file: # Read file as binary
         with open(file_path, "rb") as f:
             byte_data = f.read()
+
+        # End marker 
+        end_bin = to_bin("=====")
 
         # Get the file extension (e.g., '.txt')
         extension = file_path.split('.')[-1]
@@ -69,12 +48,12 @@ def get_payload(file_path, is_file):
 
         # Convert header and file content to binary
         header_bin = ''.join(to_bin(ord(c)) for c in header_marker)
-        file_path += "====="
         content_bin = to_bin(file_path)
 
-    return header_bin + content_bin
+    return header_bin + content_bin + end_bin
 
-def encode_audio(file_name, is_file, secret_data_file, output_name, nbits=1, start_time=None, end_time=None):
+def encode_audio(file_name, is_file, secret_data_file, output_name, n_bits=1, start_time=None, end_time=None):
+    n_bits = int(n_bits)
     with wave.open(file_name, "rb") as audio:
         params = audio.getparams()
         sample_rate = audio.getframerate()
@@ -84,7 +63,11 @@ def encode_audio(file_name, is_file, secret_data_file, output_name, nbits=1, sta
     
     frame_size = num_channels * sample_width
     total_bytes = len(frames)
-    max_bits = total_bytes * nbits
+    max_bits = total_bytes * n_bits
+
+    # convert user input time
+    start_time = to_float_or_none(start_time)
+    end_time = to_float_or_none(end_time)
 
     #convert time into frames, then into bytes
     if start_time is not None and end_time is not None:
@@ -93,6 +76,8 @@ def encode_audio(file_name, is_file, secret_data_file, output_name, nbits=1, sta
         # calculate start and end byte
         start_byte = start_frame * frame_size
         end_byte = end_frame * frame_size
+        if end_byte > total_bytes:
+            raise ValueError("[!] End time exceeds file length")
         max_bits = (end_byte - start_byte) * n_bits
     else:
         start_byte =0
@@ -102,7 +87,7 @@ def encode_audio(file_name, is_file, secret_data_file, output_name, nbits=1, sta
     binary_secret_data = get_payload(secret_data_file, is_file)
     
     while len(binary_secret_data) % n_bits !=0:
-        binary_secret_data.append(0)
+        binary_secret_data += '0'
 
     data_len = len(binary_secret_data)
     print(f"[*] Encoding message of ({data_len} bits)")
@@ -111,15 +96,6 @@ def encode_audio(file_name, is_file, secret_data_file, output_name, nbits=1, sta
         raise ValueError("[!] Insufficient bytes, need bigger image or fewer bits per channel.")
 
     print("[*] Encoding data...")
-
-    '''for i in range(0, data_len, n_bits):
-        byte_index = i // n_bits
-        bits_chunk = binary_secret_data[i:i+n_bits]
-        bits_value = int("".join(map(str, bits_chunk)), 2)
-
-        # Clear the LSBs and insert new bits
-        mask = ~((1 << nbits) - 1) & 0xFF
-        frames[byte_index] = (frames[byte_index] & mask) | bits_value'''
 
     payload_index = 0
     for i in range (start_byte, end_byte):
@@ -143,10 +119,82 @@ def encode_audio(file_name, is_file, secret_data_file, output_name, nbits=1, sta
         output.writeframes(frames)
     print(f"[+] Data encoded successfully into {output_name}")
 
+def encode_audio_with_key(file_name, is_file, secret_data_file, output_name, key, n_bits=1, start_time=None, end_time=None):
+    n_bits = int(n_bits)
+    with wave.open(file_name, "rb") as audio:
+        params = audio.getparams()
+        sample_rate = audio.getframerate()
+        sample_width = audio.getsampwidth()
+        num_channels = audio.getnchannels()
+        frames = bytearray(audio.readframes(audio.getnframes()))
+    
+    frame_size = num_channels * sample_width
+    total_bytes = len(frames)
+    max_bits = total_bytes * n_bits
+
+    # convert user input times to float or none
+    start_time = to_float_or_none(start_time)
+    end_time = to_float_or_none(end_time)
+
+    # convert time into frames, then into bytes
+    if start_time is not None and end_time is not None:
+        start_frame = int(start_time * sample_rate)
+        end_frame = int(end_time * sample_rate)
+        # calculate start and end byte
+        start_byte = start_frame * frame_size
+        end_byte = end_frame * frame_size
+        if end_byte > total_bytes:
+            raise ValueError("[!] End time exceeds file length")
+        max_bits = (end_byte - start_byte) * n_bits
+    else:
+        start_byte =0
+        end_byte = total_bytes
+            
+    # read payload file to get payload (binary)
+    binary_secret_data = get_payload(secret_data_file, is_file)
+    
+    while len(binary_secret_data) % n_bits !=0:
+        binary_secret_data += '0'
+
+    data_len = len(binary_secret_data)
+    print(f"[*] Encoding message of ({data_len} bits)")
+
+    if data_len > max_bits:
+        raise ValueError("[!] Insufficient bytes, need bigger image or fewer bits per channel.")
+
+    print("[*] Encoding data...")
+
+    # Split the secret data into chunks of n_bits length so we can shuffle it with key
+    bits_chunks =[]
+    for i in range(0, data_len, n_bits):
+        bits_chunks.append(binary_secret_data[i:i+n_bits])
+    
+    # Build shuffled order using PRNG/seed
+    order = np.arange(start_byte, end_byte, dtype = np.int64)
+    seed = hash_to_seed(key)
+    rng = np.random.Generator(np.random.PCG64(seed))
+    rng.shuffle(order)
+
+    #pad the length to match the end time so that we can decode it later
+    while len(bits_chunks) < len(order):
+        bits_chunks.append('0' * n_bits)
+
+    # Use shuffled order to encode
+    for i, pos in enumerate(order):
+        bits_value = int(bits_chunks[i],2)
+        # Clear the LSBs and insert new bits
+        mask = ~((1 << n_bits) - 1) & 0xFF
+        frames[pos] = (frames[pos] & mask) | bits_value
+
+    # Save modified audio 
+    with wave.open(output_name, "wb") as output:
+        output.setparams(params)
+        output.writeframes(frames)
+    print(f"[+] Data encoded successfully into {output_name}")
 
 if __name__ == "__main__":
     print("=== LSB Steganography Encoder ===")
-    input_file = input("Enter input BMP filename: ").strip()
+    input_file = input("Enter input WAV filename: ").strip()
     
     is_a_file = input("Is the secret a file? (Y/N): ").strip()
     
@@ -166,12 +214,17 @@ if __name__ == "__main__":
                 print("⚠️ Please enter a number between 1 and 8.")
         except ValueError:
             print("⚠️ Invalid input. Enter a number between 1 and 8.")
-
+        
     if input_file.lower().endswith(".wav"):
+        
         output_file = "encoded_output.wav"
         start = input('Enter start time in seconds:').strip()
         end = input('Enter end time in seconds:').strip()
-        encode_audio(input_file, is_file, secret_message_file, output_file, n_bits, start,end)
+        usekey = input("Use a secret key? (Y/N) ").strip()
+        if usekey.lower() == "y":
+            key = input("Enter secret key: ").strip()
+            encode_audio_with_key(input_file, is_file, secret_message_file, output_file, key, n_bits, start,end)
+        else:
+            encode_audio(input_file, is_file, secret_message_file, output_file, n_bits, start,end)
     else:
-        output_file = "encoded_output.bmp"
-        encode(input_file, secret_message_file, output_file, n_bits)
+        print("! Please enter a .wav file")
